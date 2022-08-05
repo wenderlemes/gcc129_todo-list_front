cc53618fc90ea8bae74814bbdb0f3c42512e7e3d
Wender Lemes <wender.pereira@estudante.ufla.br>
2022-08-05 00:42:58 -0300

    Componente de toggle

--- src/components/Tarefa.tsx
+++ src/components/Tarefa.tsx
@@ -1,5 +1,6 @@
 import TarefaModel from '../model/TarefaModel';
 import './Styles.css';
+import { Switch } from '@mui/material';
 import { CheckCircle, Edit, Search, ThumbsUp, Trash, X } from 'react-feather';
 
 interface TarefaProps {
@@ -15,11 +16,10 @@ const Tarefa = ({ tarefa, acaoDetalhes, acaoEditar, acaoCompletar, acaoExcluir }
         <div className="Tarefa-container">
             <div className="Tarefa-campo">{tarefa.identificacao}</div>
             <div className="Tarefa-campo">{tarefa.prazo.toLocaleString()}</div>
-            <div className="Tarefa-campo">{tarefa.completa ? <CheckCircle /> : <X /> }</div>
+            <div className="Tarefa-campo"><Switch checked={tarefa.completa} color={'default'} onChange={acaoCompletar} /></div>
             <div className="Tarefa-campo">
                 <Search cursor={'pointer'} onClick={acaoDetalhes}/>
                 <Edit cursor={'pointer'} onClick={acaoEditar}/>
-                <ThumbsUp cursor={'pointer'} onClick={acaoCompletar}/>
                 <Trash cursor={'pointer'} onClick={acaoExcluir}/>
             </div>
         </div>
